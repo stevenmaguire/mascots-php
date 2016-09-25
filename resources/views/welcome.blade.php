1ebbb42d1db0f8d1df0c2c1c53ec8471b10dc5e8
Steven Maguire <stevenmaguire@gmail.com>
2016-09-25 17:05:55 -0500

    Update welcome screen text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mascots!
                 </div>
 
                 <div class="links">
